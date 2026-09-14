c5c4d78b89d95bc57b16ecfa624c5f8ee998084e
Raden Stanislaus Airell Prakosa Sinaga <airel.prakosa@gmail.com>
2026-09-14 19:23:38 +0700

    correctly register Skill model in admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Experience, Project  # Tambahkan Education ke sini kalau lu punya model Education
+from .models import Experience, Project, Skill
 
 admin.site.register(Experience)
 admin.site.register(Project)
-admin.site.register(skills)
+admin.site.register(Skill)
