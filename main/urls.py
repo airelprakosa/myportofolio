2778ba03ad17a19784a4a57a2bc069d5281bd7ac
Raden Stanislaus Airell Prakosa Sinaga <airel.prakosa@gmail.com>
2026-09-16 16:50:35 +0700

    ngeubah uuid jadi int di url delete_project biar nggak error

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('projects/', show_projects, name='show_projects'),
     path('projects/add/', create_project, name='create_project'),
     path('api/projects/', get_projects_json, name='get_projects_json'),
-    path('projects/<uuid:project_id>/delete/', delete_project, name='delete_project'),
+    
+    path('projects/<int:project_id>/delete/', delete_project, name='delete_project'),
+    
     path('skills/', show_skills, name='show_skills'),
 ]
